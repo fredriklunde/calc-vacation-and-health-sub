8f8bd212b1db2ee5aecc5fda98ba2f7b0026b339
Fredrik Lunde <fredrik.lunde3@gmail.com>
2019-01-21 09:42:37 +0100

    Updates

---
+++ precache-manifest.a980f2c4974a8388867e15c0496eb534.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ee749a102c5a262c90b",
+    "url": "/calc-vacation-and-health-sub/static/js/main.4ee749a1.chunk.js"
+  },
+  {
+    "revision": "f6fed5e6bf7d5f9cf769",
+    "url": "/calc-vacation-and-health-sub/static/css/1.a6ba4b41.chunk.css"
+  },
+  {
+    "revision": "f6fed5e6bf7d5f9cf769",
+    "url": "/calc-vacation-and-health-sub/static/js/1.f6fed5e6.chunk.js"
+  },
+  {
+    "revision": "e8268ac1887beb5a2d9a",
+    "url": "/calc-vacation-and-health-sub/static/js/runtime~main.e8268ac1.js"
+  },
+  {
+    "revision": "bfda20d7b88beff94588e7a7c1b471e0",
+    "url": "/calc-vacation-and-health-sub/index.html"
+  }
+];
