fd830eee1b7906ab1bdaa6c9326839967df45a2f
Fredrik Lunde <fredrik.lunde3@gmail.com>
2019-01-21 08:39:39 +0100

    added date picker

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import DatePicker from 'react-date-picker';
+
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: new Date()
+    };
+    this.onDateChange = this.onDateChange.bind(this);
+  }
+ 
+  onDateChange(date) {
+    this.setState({
+      startDate: date
+    });
+  }
+    
     render(){
-        return <div> App</div>
+        return (
+            <div>
+              <h1>Select the date</h1>
+              <DatePicker
+                onChange={this.onDateChange}
+                value={this.state.startDate}
+              />
+            </div>
+          );
     }
 }
 
