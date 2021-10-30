b594d92989fcf6df7e7e449fe0ff17f556756669
Fredrik Lunde <fredrik.lunde@frontedgeit.se>
2021-10-30 22:31:08 +0200

    Refactor App.js to functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import DatePicker from "react-date-picker";
 import moment from "moment";
 import {
@@ -19,76 +19,55 @@ import {
 
 const square = { width: 200, height: 200 };
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      startingDate: null,
-      numberOfPaidVacationDays: 25,
-      healtCareSubThisYear: 0,
-      vacationDaysNextPeriod: 0,
-      message: null,
-    };
-    this.onDateChange = this.onDateChange.bind(this);
-    this.onNumberOfPaidVacationDaysChange =
-      this.onNumberOfPaidVacationDaysChange.bind(this);
-  }
+function App() {
+  const [startingDate, setStartingDate] = useState(null);
+  const [numberOfPaidVacationDays, setNumberOfPaidVacationDays] = useState(25);
+  const [healtCareSubThisYear, setHealtCareSubThisYear] = useState(0);
+  const [vacationDaysNextPeriod, setVacationDaysNextPeriod] = useState(0);
+  const [message, setMessage] = useState(null);
 
-  setHealthCareSub(startingDate) {
-    this.setState({
-      healtCareSubThisYear: calculateHealthCareSub(startingDate),
-    });
-  }
+  const setHealthCareSub = (startingDate) => {
+    const healtCareSubThisYear = calculateHealthCareSub(startingDate);
+    setHealtCareSubThisYear(healtCareSubThisYear);
+  };
 
-  setVactationDays(startingDate) {
-    this.setState({
-      vacationDaysNextPeriod: calculateNumberOfVacationDaysNextPeriod(
-        startingDate,
-        this.state.numberOfPaidVacationDays
-      ),
-    });
-  }
+  const setVactationDays = (startingDate) => {
+    const vacationDaysNextPeriod = calculateNumberOfVacationDaysNextPeriod(
+      startingDate,
+      numberOfPaidVacationDays
+    );
+    setVacationDaysNextPeriod(vacationDaysNextPeriod);
+  };
 
-  setMessage(message) {
-    this.setState({
-      message: message,
-    });
-  }
-
-  onNumberOfPaidVacationDaysChange(e) {
-    this.setState({
-      numberOfPaidVacationDays: e.target.value,
-    });
-    if (this.state.startingDate) {
-      this.setVactationDays(this.state.startingDate);
+  const onNumberOfPaidVacationDaysChange = (e) => {
+    setNumberOfPaidVacationDays(e.target.value);
+    if (startingDate) {
+      setVactationDays(startingDate);
     }
-  }
+  };
 
-  onDateChange(startingDate) {
+  const onDateChange = (startingDate) => {
     if (!startingDate) {
       return;
     }
-    this.setState({
-      startingDate: startingDate,
-    });
-    this.setHealthCareSub(startingDate);
-    this.setVactationDays(startingDate);
-    this.setMessage(
+    setStartingDate(startingDate);
+    setHealthCareSub(startingDate);
+    setVactationDays(startingDate);
+    setMessage(
       `Framtida år / perioder kommer att vara berättigade fullt bidrag och fullt antal semesterdagar.`
     );
-  }
+  };
 
-  renderMessage() {
-    if (this.state.message) {
-      return <p className="ui info message">{this.state.message}</p>;
+  const renderMessage = () => {
+    if (message) {
+      return <p className="ui info message">{message}</p>;
     }
-  }
+  };
 
-  renderVacationDaysText() {
-    if (this.state.startingDate) {
-      const yearOfFirstVacationYear = getYearOfFirstVacationPeriod(
-        this.state.startingDate
-      );
+  const renderVacationDaysText = () => {
+    if (startingDate) {
+      const yearOfFirstVacationYear =
+        getYearOfFirstVacationPeriod(startingDate);
       return (
         <Header as="h4" textAlign="center">
           Intjänade semesterdagar period 1/4/{yearOfFirstVacationYear} - 31/3/
@@ -96,10 +75,10 @@ class App extends React.Component {
         </Header>
       );
     }
-  }
+  };
 
-  renderInfoBlobs() {
-    if (this.state.startingDate) {
+  const renderInfoBlobs = () => {
+    if (startingDate) {
       return (
         <Grid stackable columns={3}>
           <Grid.Column>
@@ -108,7 +87,7 @@ class App extends React.Component {
                 <Icon name="angle right" />
                 Startdatum!
                 <Header.Subheader>
-                  {moment(this.state.startingDate).format("YYYY-MM-DD")}
+                  {moment(startingDate).format("YYYY-MM-DD")}
                 </Header.Subheader>
               </Header>
             </Segment>
@@ -121,13 +100,11 @@ class App extends React.Component {
               <Header as="h3" icon>
                 <Icon name="heart" />
                 Friskvårdsbidrag
-                <Header.Subheader>
-                  {this.state.healtCareSubThisYear}
-                </Header.Subheader>
+                <Header.Subheader>{healtCareSubThisYear}</Header.Subheader>
               </Header>
             </Segment>
             <Header as="h4" textAlign="center">
-              Friskvårdsbidrag {moment(this.state.startingDate).format("YYYY")}
+              Friskvårdsbidrag {moment(startingDate).format("YYYY")}
             </Header>
           </Grid.Column>
           <Grid.Column>
@@ -135,51 +112,43 @@ class App extends React.Component {
               <Header as="h3" icon>
                 <Icon name="sun" />
                 Semesterdagar
-                <Header.Subheader>
-                  {this.state.vacationDaysNextPeriod}
-                </Header.Subheader>
+                <Header.Subheader>{vacationDaysNextPeriod}</Header.Subheader>
               </Header>
             </Segment>
-            {this.renderVacationDaysText()}
+            {renderVacationDaysText()}
           </Grid.Column>
         </Grid>
       );
     }
-  }
+  };
 
-  render() {
-    return (
-      <Container text style={{ margin: 20 }}>
-        <Header as="h1">Friskvårdsbidragsuträknare</Header>
-        <p>
-          Applikationen räknar ut hur mycket semester du hinner jobba in till
-          nästa semesterår, samt hur mycket Friskvårdsbidrag du har rätt till
-          under kalenderåret du blev anställd.
-        </p>
-        <Header as="h5">Välj startdatum</Header>
-        <DatePicker
-          onChange={this.onDateChange}
-          value={this.state.startingDate}
-        />
-        <Header as="h5">Antal semesterdagar</Header>
-        <Input
-          label={{ basic: true, content: "Dagar" }}
-          labelPosition="right"
-          type="number"
-          size="mini"
-          value={this.state.numberOfPaidVacationDays}
-          name="quantity"
-          min="25"
-          placeholder={this.state.numberOfPaidVacationDays}
-          onChange={this.onNumberOfPaidVacationDaysChange}
-        />
-        <Divider />
-
-        {this.renderInfoBlobs()}
-        {this.renderMessage()}
-      </Container>
-    );
-  }
+  return (
+    <Container text style={{ margin: 20 }}>
+      <Header as="h1">Friskvårdsbidragsuträknare</Header>
+      <p>
+        Applikationen räknar ut hur mycket semester du hinner jobba in till
+        nästa semesterår, samt hur mycket Friskvårdsbidrag du har rätt till
+        under kalenderåret du blev anställd.
+      </p>
+      <Header as="h5">Välj startdatum</Header>
+      <DatePicker onChange={onDateChange} value={startingDate} />
+      <Header as="h5">Antal semesterdagar</Header>
+      <Input
+        label={{ basic: true, content: "Dagar" }}
+        labelPosition="right"
+        type="number"
+        size="mini"
+        value={numberOfPaidVacationDays}
+        name="quantity"
+        min="25"
+        placeholder={numberOfPaidVacationDays}
+        onChange={onNumberOfPaidVacationDaysChange}
+      />
+      <Divider />
+      {renderInfoBlobs()}
+      {renderMessage()}
+    </Container>
+  );
 }
 
 export default App;
